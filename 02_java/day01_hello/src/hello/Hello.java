151d633ff63ef67c3ed43bd26ce1548758386222
youngyuncha <chayy1515@naver.com>
2019-07-04 10:45:55 +0900

    Hello 첫 클래스 작성

---
+++ 02_java/day01_hello/src/hello/Hello.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 자바 기본 주석 
+		// 로 시작한다.
+		// 프로그램 실행에 양향을 미치지 않음.
+		
+		
+		// 자바는 대소문자가 틀리면 구문오류인 경우가
+		// 많으므로 대소문자 오타주의 하세요.
+		System.out.println("Hello, World!");
+		//실행 : ctrl + F11
+		
+		//
+
+	}
+
+}
